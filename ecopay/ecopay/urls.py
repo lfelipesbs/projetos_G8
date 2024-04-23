e2c14997e2317b96103c94fa2530699f4f6c1af5
rodrigobnm <150079017+rodrigobnm@users.noreply.github.com>
2024-04-22 22:42:57 -0300

    commit

--- ecopay/ecopay/urls.py
+++ ecopay/ecopay/urls.py
@@ -4,5 +4,9 @@ from django.urls import path, include
 from webapp import views
 
 urlpatterns = [
-    path('', views.login, name="login")
+    path('', views.login, name="login"),
+    path('home_aluno/', views.home_aluno, name="home_aluno") ,
+    path('home_adm/', views.home_adm, name="home_adm") ,
+    path('processar_formulario/', views.processar_formulario, name="processar_formulario"),
+
  ]
